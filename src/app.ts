1f01af56dad83d193b8d7fdfad2a485e9b853ea0
marlenbd <marlen@htbioimaging.com>
2024-12-15 18:48:43 +0200

    add authentication

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,15 @@ import express from 'express';
 import connectDB from './utils/db';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
+import authRoutes from './routes/auth_route'
 
 const app = express();
 const port = process.env.PORT || 3000;
+const swaggerDocument = YAML.load('./src/swagger.yaml');
 
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-const swaggerDocument = YAML.load('./src/swagger.yaml');
-
+app.use("/auth", authRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
