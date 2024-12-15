81f26e2dea9f5374a16ff0f1a316b1575d08bf16
marlenbd <marlen@htbioimaging.com>
2024-12-15 17:44:04 +0200

    add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import connectDB from './utils/db';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +11,10 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+const swaggerDocument = YAML.load('./src/swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
